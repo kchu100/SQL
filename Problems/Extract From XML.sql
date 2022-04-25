cca56a73d670e469429bbc34633e8c3f18784ecc
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-04-25 13:01:54 -0400

    Update Extract From XML.sql

--- Problems/Extract From XML.sql
+++ Problems/Extract From XML.sql
@@ -1,6 +1,6 @@
   /*
   https://app.codesignal.com/arcade/db/specialties/GvJFyTbHdFqWtXTxc
   */
-  select ExtractValue(xml_doc, '/descendant-or-self::author[1]') as author 
+  select ExtractValue(xml_doc, 'catalog/book[1]/author') as author 
 	from catalogs
 	order by author;
