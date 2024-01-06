15564997408c0dbc7ae8fa54e1f707ca0788c2c3
kchu100 <68346783+kchu100@users.noreply.github.com>
2024-01-06 01:23:47 -0500

    Update Problems.sql

--- Problems.sql
+++ Problems.sql
@@ -281,7 +281,34 @@ FROM Logs
 WHERE (Id + 1, Num) in (select * from Logs) and (Id + 2, Num) in (select * from Logs)
 
 
+/*Table: Employee
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key (column with unique values) for this table.
+departmentId is a foreign key (reference column) of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+Table: Department
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of a department and its name.
+*/
+/*Department Highest Salary*/
+select d.name, e.name, e.salary
+from employee as e inner join department as d on e.departmentid = d.id
+where (e.departmentId, e.salary) in (select departmentId, max(salary) from employee group by departmentId)
 
-
+/*Department Top 3 Highest Salary*/
 
 
