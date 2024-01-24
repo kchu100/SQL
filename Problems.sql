0517a12b13e9929348265352672921f109d2cfdf
kchu100 <68346783+kchu100@users.noreply.github.com>
2024-01-24 12:51:58 -0500

    Update Problems.sql

--- Problems.sql
+++ Problems.sql
@@ -317,3 +317,70 @@ select Department, Employee, Salary from
 ) as t1
 where r  < 4
 
+
+/*Table: Students
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| student_name  | varchar |
++---------------+---------+
+student_id is the primary key (column with unique values) for this table.
+Each row of this table contains the ID and the name of one student in the school.
+
+Table: Subjects
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| subject_name | varchar |
++--------------+---------+
+subject_name is the primary key (column with unique values) for this table.
+Each row of this table contains the name of one subject in the school.
+
+Table: Examinations
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| student_id   | int     |
+| subject_name | varchar |
++--------------+---------+
+There is no primary key (column with unique values) for this table. It may contain duplicates.
+Each student from the Students table takes every course from the Subjects table.
+Each row of this table indicates that a student with ID student_id attended the exam of subject_name.
+ 
+Write a solution to find the number of times each student attended each exam.
+Return the result table ordered by student_id and subject_name.
+*/
+select stud.student_id, stud.student_name, sub.subject_name, count(e.subject_name) as attended_exams
+from students as stud cross join subjects as sub left join examinations as e on e.student_id = stud.student_id and e.subject_name = sub.subject_name
+group by stud.student_id, stud.student_name, sub.subject_name
+order by student_id, subject_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
